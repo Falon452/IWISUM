6eeb77994253a7035de0af5253032973ae69c8e1
Falon <falon4522@gmail.com>
2024-10-26 13:02:17 +0200

    sol2

--- Lab1/ex2/solution2.py
+++ Lab1/ex2/solution2.py
@@ -3,4 +3,9 @@ from sklearn.covariance import MinCovDet
 
 
 def detect(train_data: np.ndarray, test_data: np.ndarray) -> list:
-    pass
+    mcd = MinCovDet().fit(train_data)
+    distances_train = mcd.mahalanobis(train_data)
+    max_distance = np.max(distances_train)
+    distances_test = mcd.mahalanobis(test_data)
+    artifacts = np.where(distances_test > max_distance, 1, 0)
+    return artifacts.tolist()
